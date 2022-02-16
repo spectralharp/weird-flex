e69604d9510eba355cdaa92a0b55a2cb066022e3
Chao Hsu Lin <chaohsul0226@gmail.com>
2022-02-16 16:55:54 +0800

    [feat] implement markup panel

--- src/components/BoxCSSPanel.js
+++ src/components/BoxCSSPanel.js
@@ -1,20 +1,138 @@
 import './Panel.scss';
+import Prism from "prismjs";
+import "prism-themes/themes/prism-a11y-dark.min.css";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileCode} from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
 
-export default function BoxCSSPanel() {
+export default function BoxCSSPanel({ activeBox }) {
+
+  const [cssCode, setCssCode] = useState(toCSS(activeBox));
+  const [htmlCode, setHtmlCode] = useState(toHtml(activeBox));
+
+  useEffect(() => {
+    setHtmlCode(toHtml(activeBox, 0));
+    setCssCode(toCSS(activeBox, 0));
+  }, [activeBox])
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [cssCode, htmlCode]);
+
   return (
     <>
       <h2 className="title"><FontAwesomeIcon icon={faFileCode} /> Markup</h2>
       <p>
-        Shows the CSS markup for the active box
+        Shows the markup for the active box
       </p>
 
-      <div id="url-bar">
-        <input type="text" id="url-output" />
-        <button id="get-url-btn">Get URL</button>
-      </div>
+      <pre>
+        <code className={'language-html'}>{htmlCode}</code>
+      </pre>
+
+      <pre>
+        <code className={'language-css'}>{cssCode}</code>
+      </pre>
     </>
   );
+}
+
+function toHtml(root, indent) {
+  if(!root) return '';
+  const indentation = ' '.repeat(indent);
+  switch(root.camouflage) {
+    case 'title':
+      return indentation + `<h1${indent === 0 ? ' id="box"' : ''}>Title</h1>`;
+    case 'paragraph':
+      return indentation + `<p${indent === 0 ? ' id="box"' : ''}>Lorem Ipsum...</p>`;
+    case 'image':
+      return indentation + `<img${indent === 0 ? ' id="box"' : ''} src="placeholder.png" alt="placeholder"/>`;
+    default:
+      return `${indentation}<div${indent === 0 ? ' id="box"' : ''}>${root.nodes ? '\n' + root.nodes.map(node => toHtml(node, indent + 2)).join('\n') + `\n${indentation}` : ''}</div>`;
+  }
+}
+
+function toCSS(root, indent) {
+  if(!root) return '';
+  const { width, height } = root;
+  let { style } = root;
+  if(!style) {
+    style = {};
+  }
+
+  const {
+    display, flexDirection, flexWrap, justifyContent, alignItems, alignContent,
+    alignSelf, order, flexGrow, flexShrink, flexBasis,
+    writingMode
+  } = style;
+
+  let code = '';
+
+  if (width) {
+    code += `  width: ${width}px;\n`
+  }
+  if (height) {
+    code += `  height: ${height}px;\n`
+  }
+
+  if (display) {
+    code += `  display: ${display};\n`
+  }
+
+  if (flexDirection) {
+    if(flexWrap) {
+      code += `  flex-flow: ${flexDirection} ${flexWrap};\n`
+    } else {
+      code += `  flex-direction: ${flexDirection};\n`
+    }
+  } else if(flexWrap) {
+    code += `  flex-wrap: ${flexWrap};\n`
+  }
+
+  if (justifyContent) {
+    code += `  justify-content: ${justifyContent};\n`
+  }
+
+  if (alignItems) {
+    code += `  align-items: ${alignItems};\n`
+  }
+
+  if (alignContent) {
+    code += `  align-content: ${alignContent};\n`
+  }
+
+
+
+  if (alignSelf) {
+    code += `  align-self: ${alignSelf};\n`
+  }
+
+  if (order) {
+    code += `  order: ${order};\n`
+  }
+
+  if(flexGrow && flexShrink && flexBasis) {
+    code += `  flex: ${flexGrow} ${flexShrink} ${flexBasis};\n`
+  } else {
+    if (flexGrow) {
+      code += `  flex-grow: ${flexGrow};\n`
+    }
+
+    if (flexShrink) {
+      code += `  flex-shrink: ${flexShrink};\n`
+    }
+
+    if (flexBasis) {
+      code += `  flex-basis: ${flexBasis};\n`
+    }
+  }
+
+
+
+  if (writingMode) {
+    code += `  writing-mode: ${writingMode};\n`
+  }
+
+  return `#box {\n${code}}`;
 }
